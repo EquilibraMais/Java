6bb7e70b75224464e9d8dc7805250239b5e5b45f
Matheus de Freita Silva <Matheus.freitas_silva@outlook.com>
2025-11-23 14:42:12 -0300

    Criado regra de internacionalização

---
+++ src/main/java/com/gs/equilibramais/internacionalizacao/InternacionalizacaoConfig.java
@@ -0,0 +1,43 @@
+package com.gs.equilibramais.internacionalizacao;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+@Configuration
+public class InternacionalizacaoConfig implements WebMvcConfigurer {
+
+	@Bean
+	public ReloadableResourceBundleMessageSource messageSource() {
+		ReloadableResourceBundleMessageSource ms = new ReloadableResourceBundleMessageSource();
+		ms.setBasename("classpath:mensagens");
+		ms.setDefaultEncoding("UTF-8");
+		return ms;
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		CookieLocaleResolver lr = new CookieLocaleResolver();
+		lr.setDefaultLocale(Locale.forLanguageTag("pt"));
+		return lr;
+	}
+	
+	@Bean
+	public LocaleChangeInterceptor changeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(changeInterceptor());
+	}
+}
